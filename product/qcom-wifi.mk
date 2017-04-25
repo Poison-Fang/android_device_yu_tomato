bcb526a816a0886e8a38e20bf78833b823cc12a9
Poison-Fang <ayushwalia999@gmail.com>
2017-04-25 20:21:57 +0530

    Enable Wifi Calling

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -5,3 +5,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+# Enable Wifi Calling
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.dbg.wfc_avail_ovr=1
