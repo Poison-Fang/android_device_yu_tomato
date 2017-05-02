69256919bc806a5371c30bb84b6800416decdcee
Poison-Fang <ayushwalia999@gmail.com>
2017-05-02 10:45:47 +0530

    Add IMS Props

---
+++ product/ims.mk
@@ -0,0 +1,18 @@
+# IMS Props
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.lte.volte_call_capable=true \
+    persist.ims.enableADBLogs=1 \
+    persist.ims.enableDebugLogs=1 \
+    persist.radio.calls.on.ims=true \
+    persist.radio.csvt.enabled=false \
+    persist.radio.jbims=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.volte_enabled_by_hw=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    DEVICE_PROVISIONED=1 \
+    ril.subscription.types=NV,RUIM \
+    telephony.lteOnCdmaDevice=1
