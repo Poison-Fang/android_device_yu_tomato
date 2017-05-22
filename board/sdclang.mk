ca97697eac96b32548b29901fed4b6b9764cc976
Poison-Fang <ayushwalia999@gmail.com>
2017-05-22 14:20:03 +0000

    Qcom SDllvm

---
+++ board/sdclang.mk
@@ -0,0 +1,6 @@
+#Qualcomm SDllvm
+ifneq ($(HOST_OS),darwin) 
+SDCLANG := true 
+SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin 
+SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
+endif
