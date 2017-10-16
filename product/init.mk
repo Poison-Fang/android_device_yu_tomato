f417c5e0cb7eccc17e8345982c364ce1786184aa
Poison-Fang <ayushwalia999@gmail.com>
2017-10-16 15:16:01 +0530

    Enable deep_sleep

--- product/init.mk
+++ product/init.mk
@@ -3,4 +3,3 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc \
     init.qti.ims.sh
-
---
+++ product/tweaks.mk
@@ -0,0 +1,4 @@
+# Deep Sleep
+PRODUCT_PROPERTY_OVERRIDES += \
+    pm.sleep_mode=1 \
+    ro.ril.disable.power.collapse=1
